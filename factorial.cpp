e228fda688ca48aff6ec990212cc4bb7a158aa56
sunnyxiu <61191922+sunnyxiu@users.noreply.github.com>
2024-03-08 16:32:30 +0800

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,27 @@
+﻿
+#include<iomanip>
+#include <iostream>
+using std::setw;
+using std::cout;
+unsigned long factorial(unsigned long a) {
+    if (a <= 1) { return 1; }
+    a = a * factorial(a - 1);
+    return a;
+}
+
+int main()
+{
+    //n!=n*(n-1)!
+    /*
+    int total{1};
+    for (unsigned int counter{ 1 }; counter <= 5; counter++) {
+        total *= counter;
+    }
+    */
+    
+    for (int num{ 1 }; num <= 10; num++) {
+        cout << setw(2)<<num << "! = " << setw(10)<<factorial(num)<<std::endl;
+    }
+    
+}
+
