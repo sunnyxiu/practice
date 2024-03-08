54649d58edbe1bd189b34962be59050db5728a62
unknown <z99876664@email.com>
2024-03-08 15:21:40 +0800

    first commit

---
+++ algorithm/algorithm/algorithm.cpp
@@ -0,0 +1,32 @@
+﻿// algorithm.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include<cstdlib>
+
+
+int main()
+{
+    int q{ 0 };
+    int a;
+    unsigned int d;
+    std::cin >> a >> d;
+    unsigned int r = abs(a);
+    while (r >= d) {
+        r -= d;
+        q++;
+    }
+    if (a < 0 && r == 0) {
+        q = -1*q;
+    }
+    if (a < 0 && r>0) {
+        r = d - r;
+        q = -1-q;
+    }
+        
+    std::cout << "(" << q << "," << r << ")";
+
+
+}
+
+
