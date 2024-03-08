1f209ab0fca073d2b28e9bf27f24ba7f74305efe
sunnyxiu <61191922+sunnyxiu@users.noreply.github.com>
2024-03-08 16:51:42 +0800

    Add files via upload

---
+++ Range-based for statement.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include<array>
+using std::cout;
+using std::array;
+
+int main()
+{
+    array<int, 5>arrayname={1,2,3,4,5};
+    for (int item : arrayname) {
+        cout << item << " ";
+    }
+
+    for (int& itemRef : arrayname) {
+        itemRef *= 2;
+    }
+
+    for (int item : arrayname) {
+        cout << item << " ";
+    }
+}
+
---
+++ Reference.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using std::string;
+const std::string& getName(std::string name) {
+	static string tired = name;
+	return tired;
+};
+
+int cubic(int x, int y=1, int z=1) {
+	return x * y * z;
+};
+int main() {
+	std::string sunny = "angry";
+	std::cout<<getName(sunny)<<std::endl;
+	std::cout << cubic(1, 2);
+
+
+}
+
